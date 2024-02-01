6bc72e644fdf7d3c52f37d8caf3fca835ad90f8a
Simon Rumble <simon@simonrumble.com>
2024-02-01 14:10:23 +1100

    Fix CDN issue and leading line break in HTML file

--- sync_to_s3.sh
+++ sync_to_s3.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-s3cmd sync --acl-public --preserve --recursive --exclude-from=.gitignore ./ s3://contractorpermie.com/
-aws cloudfront create-invalidation --distribution-id E1ZW5KU8M6GM50 \
---invalidation-batch "{ \"Paths\": { \"Quantity\": 1, \"Items\": [ \"/*\" ] }, \"CallerReference\": \"`date +%s`\" }"
+s3cmd sync --acl-public --preserve --recursive --exclude-from=.gitignore ./ s3://www.contractorpermie.com/
+
+# Turns out I'm using Fastly not Cloudfront so don't need this
+# aws cloudfront create-invalidation --distribution-id E1ZW5KU8M6GM50 \
+# --invalidation-batch "{ \"Paths\": { \"Quantity\": 1, \"Items\": [ \"/*\" ] }, \"CallerReference\": \"`date +%s`\" }"
